f5219a56903bb85a11480a6283401051e95a1fa6
Tasha Upchurch <tasha@hoppingmadgames.com>
2025-09-25 02:26:19 +0000

    Sync HTML renderer input and preview height

--- js/htmlRenderer.js
+++ js/htmlRenderer.js
@@ -5,6 +5,16 @@ app.registerExtension({
     name: "Comfy.HTMLRenderer",
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
         if (nodeData.name === "HTMLRenderer" && nodeData.category === "ETK/LLM/LiteLLM") {
+            const applyIframeHeight = function () {
+                if (!this.htmlRendererDisplayEl) {
+                    return;
+                }
+
+                const heightWidget = this.widgets?.find?.((w) => w.name === "iframe_height");
+                const rawValue = typeof heightWidget?.value === "string" ? heightWidget.value.trim() : "";
+                this.htmlRendererDisplayEl.style.height = rawValue || "auto";
+            };
+
             // Node Created
             const onNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function () {
@@ -18,7 +28,6 @@ app.registerExtension({
                         this,
                         "html_content",
                         ["STRING", {
-                            multiline: true,
                             default: "",
                             placeholder: "HTML content..."
                         }],
@@ -29,7 +38,8 @@ app.registerExtension({
                 // Create HTML display widget
                 const displayEl = document.createElement("div");
                 displayEl.style.width = "100%";
-                displayEl.style.height = "200px";
+                displayEl.style.minHeight = "200px";
+                displayEl.style.height = "auto";
                 displayEl.style.overflow = "auto";
                 displayEl.style.border = "1px solid #ddd";
                 displayEl.style.borderRadius = "5px";
@@ -46,33 +56,54 @@ app.registerExtension({
                     },
                 });
 
+                this.htmlRendererDisplayEl = displayEl;
+
                 this.setSize([350, 400]);
+
+                applyIframeHeight.call(this);
 
                 return ret;
             };
 
             // Function to update display
             const updateDisplay = function (html_content) {
-                if (html_content && html_content.length > 0) {
+                if (typeof html_content === "string") {
                     this.displayWidget.value = html_content;
-                    app.graph.setDirtyCanvas(true);
+                    if (html_content.length > 0) {
+                        app.graph.setDirtyCanvas(true);
+                    }
                 }
+
+                applyIframeHeight.call(this);
             };
 
             // onExecuted
             const onExecuted = nodeType.prototype.onExecuted;
             nodeType.prototype.onExecuted = function (message) {
                 onExecuted?.apply(this, arguments);
-                updateDisplay.call(this, message.string[0]);
+                const html = message?.string?.[0];
+                updateDisplay.call(this, html);
             };
 
             // onConfigure
             const onConfigure = nodeType.prototype.onConfigure;
             nodeType.prototype.onConfigure = function (config) {
                 onConfigure?.apply(this, arguments);
-                if (config?.widgets_values?.length) {
-                    updateDisplay.call(this, config.widgets_values[0]);
+                const configuredHtml = config?.widgets_values?.[0];
+                if (typeof configuredHtml === "string") {
+                    updateDisplay.call(this, configuredHtml);
+                } else {
+                    applyIframeHeight.call(this);
                 }
+            };
+
+            const onWidgetChanged = nodeType.prototype.onWidgetChanged;
+            nodeType.prototype.onWidgetChanged = function (widget, value, ...args) {
+                const ret = onWidgetChanged?.apply(this, [widget, value, ...args]);
+                if (widget?.name === "iframe_height") {
+                    applyIframeHeight.call(this);
+                }
+                return ret;
             };
         }
     },
